581b6db92ddcd3f781c9af07c8a853156b46baa0
dust <834902408@qq.com>
2019-11-14 22:17:07 +0800

    new file:   src/main/java/com/dust/copybean/annotation/CopyBean.java 	new file:   src/main/java/com/dust/copybean/annotation/CopyName.java

---
+++ src/main/java/com/dust/copybean/annotation/CopyName.java
@@ -0,0 +1,19 @@
+package com.dust.copybean.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记要复制的实体的变量的变量名，copybean在复制的时候会根据改注解的变量名进行查找复制。
+ *
+ * 同时也只会对添加了该注解的变量进行复制
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CopyName {
+
+    String target() default "";
+
+}
